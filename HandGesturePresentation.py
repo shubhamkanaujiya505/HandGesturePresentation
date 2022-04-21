ce1b9f06051e4ee83c7184fcc74cbc5c349fd1c2
Shubham Kanaujiya <shubhamkanaujiya505@gmail.com>
2022-04-22 00:42:39 +0530

    import images

--- HandGesturePresentation.py
+++ HandGesturePresentation.py
@@ -1,15 +1,23 @@
 from sre_constants import SUCCESS
 import cv2
+import os
 
 # Variables
 width, height = 50, 20 # camera size adjust
+folderPath = "Presentation"
 
 #camera setup
 cap = cv2.VideoCapture(0)
 cap.set(3, width)
 cap.set(4, height)
 
+# Get the list of presentation images
+pathImages = os.listdir(folderPath)
+print(pathImages)
+
 while True:
+    # import images
+
     success, img = cap.read()
     cv2.imshow("image", img)
     key = cv2.waitKey(1)
